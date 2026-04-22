17acbfdf36e848261e01efee6b0e81ec078fe755
AriSnowie97 <diysylveon@gmail.com>
2026-04-22 18:18:28 +0300

    client choosed style, so we adapted site for this style

--- script.js
+++ script.js
@@ -17,19 +17,3 @@ if (burgerBtn && navContainer) {
         });
     });
 }
-// Функция для изменения стиля (темы)
-function changeTheme(themeFile) {
-    // Находим тег <link> по его id и меняем путь к CSS-файлу
-    document.getElementById('theme-link').setAttribute('href', themeFile);
-    
-    // Сохраняем выбор в память браузера (localStorage)
-    localStorage.setItem('selectedTheme', themeFile);
-}
-
-// Проверяем, есть ли сохраненный стиль при загрузке страницы
-window.onload = () => {
-    const savedTheme = localStorage.getItem('selectedTheme');
-    if (savedTheme) {
-        document.getElementById('theme-link').setAttribute('href', savedTheme);
-    }
-}
